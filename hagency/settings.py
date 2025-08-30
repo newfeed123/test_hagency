a036b329d33b9a78994982f3955b2c8333a6b278
huytn <tranngochuy12ctm1@gmail.com>
2025-08-30 16:29:55 +0700

    update

--- hagency/settings.py
+++ hagency/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rrn7*1-8(y*zsy+eqpjot1ekbcl+v2(=c(_rlusle8hvp7%zi=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['test-hagency.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
